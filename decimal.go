d59f8ebc35d650dc688847b0ed4e1e6a81614ce5
José Alves <jmcalves275@gmail.com>
2019-01-03 19:28:53 +0000

    Revert "marshal integers with 2 decimal places"
    This reverts commit f1e4694858a55b3fc4eb41a1a339466c68acf47c.

--- decimal.go
+++ decimal.go
@@ -51,8 +51,6 @@ var DivisionPrecision = 16
 // double-precision floating point numbers, which means you can potentially
 // silently lose precision.
 var MarshalJSONWithoutQuotes = false
-var MarshalJSONWithDecimalPlaces = true
-var MarshalJSONDecimalPlaces int32 = 2
 
 // Zero constant, to make computations faster.
 var Zero = New(0, 1)
@@ -931,18 +929,11 @@ func (d *Decimal) UnmarshalJSON(decimalBytes []byte) error {
 
 // MarshalJSON implements the json.Marshaler interface.
 func (d Decimal) MarshalJSON() ([]byte, error) {
-	var stringVal string
-	if MarshalJSONWithDecimalPlaces {
-		stringVal = d.StringFixed(MarshalJSONDecimalPlaces)
-	} else {
-		stringVal = d.String()
-	}
-
 	var str string
 	if MarshalJSONWithoutQuotes {
-		str = stringVal
+		str = d.String()
 	} else {
-		str = "\"" + stringVal + "\""
+		str = "\"" + d.String() + "\""
 	}
 	return []byte(str), nil
 }
